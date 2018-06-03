f2e6853e6b318a567565bc940e8994d4f7d8be8a
XnnChen <39849822+XnnChen@users.noreply.github.com>
2018-06-03 15:30:06 +0800

    Update test6_黄金分割数.py

--- codeEx/test6_黄金分割数.py
+++ codeEx/test6_黄金分割数.py
@@ -1,1 +1,20 @@
-ss
+def fib(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return fib(n - 1) + fib(n -2)
+
+
+def main():
+    """使用斐波那契数列，求出精确到 0.00001 的黄金分割数"""
+    a = fib(19)
+    b = fib(18)
+
+    ans = b / a
+    
+    print("%.5f" % ans)
+    # print(a)
+
+
+if __name__ == "__main__":
+    main()
