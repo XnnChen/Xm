d54a0c21c7cc15308ec5d8a2270611afde51d899
XnnChen <39849822+XnnChen@users.noreply.github.com>
2018-06-03 14:25:50 +0800

    Update test3_斐波那契.py

--- codeEx/test3_斐波那契.py
+++ codeEx/test3_斐波那契.py
@@ -1,12 +1,11 @@
 def main():
     """打印前 20 项斐波那契数列：a_1=1, a_2 = 1,a_n = a_n-1 + a_n-2"""
-    a1, a2 = 1, 1
-    i = 0
-    while i < 20:
-        i += 1
-        if i > 1:
-            a1, a2 = a2, a1 + a2
-        print(a1)
+    fib = [101]
+    fib[0] = 1
+    fib[1] = 1
+    for i in fib:
+
+        fib[i] = fib[i - 1] + fib[i - 2]
 
 
 if __name__ == "__main__":
