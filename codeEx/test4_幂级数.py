b8dafd512ca5ff78383fd05733ec3c49dd14d951
XnnChen <39849822+XnnChen@users.noreply.github.com>
2018-06-03 13:19:36 +0800

    Update test4_幂级数.py

--- codeEx/test4_幂级数.py
+++ codeEx/test4_幂级数.py
@@ -13,19 +13,18 @@ def X(x, n):
     return x ** n / f(n)
 
 
-def main():
+def main(a_input):
     """对于幂级数：e^x = 1 + x + x^2/2! + x^3/3!+ … 分别求出前 2、5、10 项的求和结果"""
     x = float(input("请输入x的值:"))
-    x_sum = 0
-    for a in range(11):
-        if a == 2 or a == 5 or a == 10:
-            for y in range(a):
+    result = 0
 
-                x_sum += X(x, y)
-
-            print("The first {} item sum is {}".format(a, x_sum))
-            return x_sum
+    for a in range(a_input):
+        result += X(x, a)
+    print("The first {} item sum is {}".format(a_input, result))
+    return result
 
 if __name__ == "__main__":
 
-    main()
+    for a in range(11):
+        if a == 2 or a == 5 or a == 10:
+            main(a)
